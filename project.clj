a5a6bd20d0fb57b30e7e599043344921642dad34
Rodrigo Nossal <rodrigo@noss.al>
2016-06-16 01:31:23 -0300

    updating & cleaning

--- project.clj
+++ project.clj
@@ -1,40 +1,42 @@
-(defproject nossal "1.0.0-SNAPSHOT"
+(defproject nossal "1.0.1-SNAPSHOT"
   :description "Nossal's personal web app"
   :url "nossal.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [environ "0.5.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [environ "1.0.3"]
                  [hiccup "1.0.5"]
-                 [org.clojure/clojurescript "0.0-3211"]
-                 [lein-figwheel "0.3.1"]
-                 [garden "1.2.5"]]
+                 [org.clojure/clojurescript "1.9.36"]
+                 [garden "1.3.2"]]
 
   :ring {:handler nossal.web/app :auto-refresh? true}
   :min-lein-version "2.0.0"
 
-  :plugins [[environ/environ.lein "0.2.1"]
-            [lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.3.1"]
-            [lein-garden "0.2.5"]
-            [lein-ring "0.9.3"]]
+  :plugins [[environ/environ.lein "0.3.1"]
+            [lein-cljsbuild "1.1.3"]
+            [lein-figwheel "0.5.4-3"]
+            [lein-garden "0.2.8"]
+            [lein-ring "0.9.7"]]
 
   :hooks [environ.leiningen.hooks]
   :uberjar-name "nossal.jar"
   :profiles {:production {:env {:production true}}}
   ;:prep-tasks [["garden" "once"]]
+  :figwheel {
+    :css-dirs [ "resources/public/css" ]}
 
-;  :cljsbuild {:builds [{:id "nossal"
-;                        :source-paths ["src/"]
-;                        :figwheel {:on-jsload "nossal.web/reload-hook"}
-;                        :compiler {:main "nossal.web"
-;                                   :asset-path "js/out"
-;                                   :output-to "resources/public/js/app.js"
-;                                   :output-dir "resources/public/js/out"
-;                                   :source-map-timestamp true
-;                                   :source-map true}}]}
+ :cljsbuild {:builds [{:id "nossal"
+                       :source-paths ["src/"]
+                       :figwheel {:on-jsload "nossal.web/fig-reload"}
+                       :compiler {:main "nossal.web"
+                                  :output-to "resources/public/js/app.js"
+                                  :output-dir "resources/public/js/out"
+                                  :optimizations :none
+                                  :source-map true
+                                  :source-map-timestamp true
+                                  :asset-path "js/out"}}]}
 
   :garden {:builds [{;; Optional name of the build:
                      :id "screen1"
--- src/nossal/web.clj
+++ src/nossal/web.clj
@@ -8,6 +8,7 @@
             [hiccup.core :as h]
             [hiccup.page :as page]))
 
+
 (def google-analytics [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -15,57 +16,69 @@
   ga('create', 'UA-11532471-6', 'auto');
   ga('send', 'pageview');"])
 
+
 (defn base [title css body js-code]
   (page/html5 {:lang "pt-br"}
-   [:head
-    [:meta {:charset "UTF-8"}]
-    [:meta {:http-equiv "cleartype" :content "on"}]
-    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"}]
-    [:meta {:name "keywords" :content "java, clojure, clojurescript, javascript, python, programming, development, html5, html, css, ajax, device orientation, instant, fast, dinherin, dinher.in"}]
-    [:meta {:name "description" :content "Site pessoal de Rodrigo Nossal"}]
-    [:meta {:name "google-site-verification" :content "ljuFr_e6LgEvNLMWoyGBPxvcmCQQQkwY28VpiKz3Eb8"}]
-    [:title title]
-    [:link {:rel "stylesheet" :href "//fonts.googleapis.com/css?family=Raleway:100,300,800"}]
-    [:link {:rel "stylesheet" :href "/css/screen.css" :type "text/css"}]
-    [:style css]]
-   [:body (seq body)
-    [:footer [:span.made
-              "Handmade with full stack " [:a {:href "http://clojure.org" :target "_blank"} "Clojure"] " and " [:span.heart " ♥ "] " in " [:a {:href "//pt.wikipedia.org/wiki/Gravata%C3%AD" :target "_blank"} "Grav."]]]
+    [:head
+      [:meta {:charset "UTF-8"}]
+      [:meta {:http-equiv "cleartype" :content "on"}]
+      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"}]
+      [:meta {:name "keywords" :content "java, clojure, clojurescript, javascript, python, programming, development, html5, html, css, ajax, device orientation, instant, fast, dinherin, dinher.in"}]
+      [:meta {:name "description" :content "Site pessoal de Rodrigo Nossal"}]
+      [:meta {:name "google-site-verification" :content "ljuFr_e6LgEvNLMWoyGBPxvcmCQQQkwY28VpiKz3Eb8"}]
+      [:title title]
+      [:link {:rel "stylesheet" :href "/css/screen.css" :type "text/css"}]
+      [:style css]]
+    [:body (seq body)
+    [:footer
+      [:span.made "Handmade with full stack "
+        [:a {:href "http://clojure.org" :target "_blank"} "Clojure"] " and "
+        [:span.heart " ♥ "] " in "
+        [:a {:href "//pt.wikipedia.org/wiki/Gravata%C3%AD" :target "_blank"} "Grav."]]]
     [:script {:type "text/javascript"} js-code] google-analytics]))
+
 
 (defn index []
   (base "Rodrigo Nossal" "lll"
-        [[:header {:itemscope "" :itemtype "http://data-vocabulary.org/Person"}
-          [:span.name [:h1 {:itemprop "name"} "Rodrigo Nossal"]]]
-         [:section#me [:div#tweetwidget]
-         [:a.start {:href "#"}
-          [:svg {:width "60" :height "30" :xmlns "http://www.w3.org/2000/svg"}
-           [:g#svg_1
+    [[:header {:itemscope "" :itemtype "http://data-vocabulary.org/Person"}
+      [:span.name [:h1 {:itemprop "name"} [:span "Rodrigo Nossal"]]]]
+    [:section#me [:div#tweetwidget]
+      [:a.start {:href "#"}
+        [:svg {:width "60" :height "30" :xmlns "http://www.w3.org/2000/svg"}
+          [:g#svg_1
             [:line {:y2 "24" :x2 "30" :y1 "5" :x1 "4" :stroke-linecap "round" :stroke-width "8"}]
             [:line {:y2 "24" :x2 "30" :y1 "5" :x1 "56" :stroke-linecap "round" :stroke-width "8"}]]]]]
-         [:div.divisor]
-         [:section#facebook]
-         #_[:section#photos
-          [:h2 "Minhas 	Fotos"]
-          [:div#instaphotos.gallery]
-          [:div#flickrphotos.gallery]]
-         #_[:section#contact
-          [:form {:method "POST"}
-           [:h2 "Vamos conversar!"]
-           [:input {:type "email" :name "email" :placeHolder "seu@email.com"}]
-           [:textarea {:name "message" :rows "5"}]
-           [:input {:type "submit" :value "Enviar Mensagem"}]]]
-         #_[:section#end [:div.end "Fim."]]
-         ]
-        "console.log('Oi!')"))
+    [:div.divisor]
+    [:section#facebook]
+    #_[:section#photos
+      [:h2 "Minhas 	Fotos"]
+      [:div#instaphotos.gallery]
+      [:div#flickrphotos.gallery]]
+    #_[:section#contact
+      [:form {:method "POST"}
+        [:h2 "Vamos conversar!"]
+        [:input {:type "email" :name "email" :placeHolder "seu@email.com"}]
+        [:textarea {:name "message" :rows "5"}]
+        [:input {:type "submit" :value "Enviar Mensagem"}]]]
+    #_[:section#end [:div.end "Fim."]]
+    ]
+    "console.log('Oi!')"))
+
+
+(defn dot [req]
+  (str req))
+
 
 (defroutes app
   (GET "/" []
-       (index))
+    (index))
+  (GET "/dot" request
+    (dot request))
   (route/resources "/")
   (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
+    (route/not-found (slurp (io/resource "404.html")))))
+
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
