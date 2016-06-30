e06e1250df2d46a0f9685ec037e38a0696f01e41
Rodrigo Nossal <rodrigo@noss.al>
2016-06-30 02:59:29 -0300

    css heart

--- src/nossal/web.clj
+++ src/nossal/web.clj
@@ -38,7 +38,7 @@
      [:footer
        [:span.made "Handmade " [:a {:href "https://github.com/nossal/noss.al", :target "_blank"} "entirely"] " in "
          [:a {:href "http://clojure.org" :target "_blank"} "Clojure"] " and "
-         [:span.heart " ♥ "] " at "
+         [:span.heart " "] " at "
          [:a {:href "//pt.wikipedia.org/wiki/Gravata%C3%AD" :target "_blank"} "Grav."]]]
      [:script {:type "text/javascript"} js-code] google-analytics]))
 
@@ -46,7 +46,8 @@
 (defn index []
   (base "Rodrigo Nossal" "lll"
     [[:header {:itemscope "" :itemtype "http://data-vocabulary.org/Person"}
-      [:span.name [:h1 {:itemprop "name"} [:span "Rodrigo Nossal"]]]]
+      [:span.name [:h1 {:itemprop "name"} [:span "Rodrigo Nossal"]]]
+      [:p.about-line "Full-Stack Developer"]]
      [:section#me [:div#tweetwidget]
        [:a.start {:href "#"}
          [:svg {:width "60" :height "30" :xmlns "http://www.w3.org/2000/svg"}
@@ -85,7 +86,7 @@
     (base "dotfiles" ""
       [[:header [:h1 "dotfiles"]
         [:p.catch "A terminal configuration system"]]
-       [:section [:div.terminal "eval " [:span.string "\"$(curl -fsL noss.al/dot)\""]]]] "")))
+       [:section [:div.terminal "eval " [:span.string "\"$(curl -sL noss.al/dot)\""]]]] "")))
 
 
 (defroutes app
