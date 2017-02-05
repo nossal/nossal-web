4f8a1c92223abd3775d9b5287203d83a9a1bd26f
nossal <rodrigo@noss.al>
2017-02-05 02:02:56 -0200

    ..

---
+++ src/nossal/cljs/app.cljs
@@ -0,0 +1,13 @@
+(ns nossal.cljs.app
+
+ (:require-macros [cljs.core.async.macros :refer [go]])
+ (:require [goog.events :as events]
+           [cljs.core.async :refer [<! chan]]
+           [om.core :as om :include-macros true]
+           [om.dom :as dom :include-macros true]
+           [secretary.core :as secretary]
+           [clojure.string :as string])
+ (:import [goog History]
+          [goog.history EventType]))
+
+(js/alert "Hello from ClojureScript!!!")
