1c1e06519a17d98ae31413397644a3da197a3a77
Rodrigo Nossal <rodrigo@noss.al>
2017-02-13 03:02:55 -0200

    trying to update things

--- project.clj
+++ project.clj
@@ -4,23 +4,25 @@
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.0"]
-                 [ring/ring-jetty-adapter "1.5.0"]
-                 [environ "1.0.3"]
+                 [compojure "1.5.2"]
                  [hiccup "1.0.5"]
-                 [org.clojure/clojurescript "1.9.36"]
+                 [environ "1.0.3"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+
+                 [org.clojure/clojurescript "1.9.456"]
+                 [reagent "0.6.0"]
                  [garden "1.3.2"]
                  [clj-http "3.1.0"]]
 
-  :ring {:handler nossal.web/app :auto-refresh? true}
-  :min-lein-version "2.0.0"
-
   :plugins [[environ/environ.lein "0.3.1"]
             [lein-cljsbuild "1.1.3"]
-            [lein-figwheel "0.5.4-3"]
-            [lein-garden "0.2.8"]
-            [lein-ring "0.9.7"]]
+            [lein-figwheel "0.5.9"]
+            [lein-garden "0.3.0"]
+            [lein-ring "0.11.0"]]
 
+  :min-lein-version "2.7.0"
+
+  :ring {:handler nossal.web/app :auto-refresh? true}
   :hooks [environ.leiningen.hooks]
   :uberjar-name "nossal.jar"
   :profiles {:production {:env {:production true}}}
@@ -28,9 +30,9 @@
   :figwheel {:css-dirs [ "resources/public/css"]}
 
   :cljsbuild {:builds [{:id "nossal"}
-                       :source-paths ["src/"]
+                       :source-paths ["src/cljs"]
                        :figwheel {:on-jsload "nossal.web/fig-reload"}
-                       :compiler {:main "nossal.web"
+                       :compiler {:main "nossal.clj.app"
                                   :output-to "resources/public/js/app.js"
                                   :output-dir "resources/public/js/out"
                                   :optimizations :none
@@ -41,7 +43,7 @@
   :garden {:builds [{;; Optional name of the build:
                      :id "screen1"
                      ;; Source paths where the stylesheet source code is
-                     :source-paths ["src/"]
+                     :source-paths ["src/styles"]
                      ;; The var containing your stylesheet:
                      :stylesheet styles.core/screen
                      ;; Compiler flags passed to `garden.core/css`:
