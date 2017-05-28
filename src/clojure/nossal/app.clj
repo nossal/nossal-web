88c59d9113fb2ff8ff58613175fc14269dd6013d
nossal <rodrigo@noss.al>
2017-05-28 17:38:45 -0300

    Revert "gzip"
    This reverts commit af53a5cf2a785181bf0b7e1ef187f4132531caf6.

--- src/clojure/nossal/app.clj
+++ src/clojure/nossal/app.clj
@@ -5,8 +5,7 @@
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
             [clojure.java.io :as io]
-            [compojure.handler :refer [site]])
-  (:import [org.eclipse.jetty.server.handler.gzip GzipHandler]))
+            [compojure.handler :refer [site]]))
 
 
 (defroutes app
@@ -28,19 +27,6 @@
     (route/not-found (slurp (io/resource "404.html")))))
 
 
-(defn- add-gzip-handler [server]
-  (.setHandler server
-               (doto (GzipHandler.)
-                 (.setIncludedMimeTypes (into-array ["text/css"
-                                                     "text/plain"
-                                                     "text/javascript"
-                                                     "application/javascript"
-                                                     "application/json"
-                                                     "image/svg+xml"]))
-                 (.setMinGzipSize 1024)
-                 (.setHandler (.getHandler server)))))
-
-
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
-    (jetty/run-jetty (site #'app) {:port port :join? false :configurator add-gzip-handler})))
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
