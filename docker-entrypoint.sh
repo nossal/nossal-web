197b44f7c0f733f1dfbf923d21f6f644d08f7db2
nossal <rodrigo.nossal@corp.terra.com>
2017-10-09 17:13:29 -0300

    docker fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,15 @@
 set -ex
 
 export JVM_OPTS="-Xms50m -Xmx100m -Dfile.encoding=UTF-8"
-# export PORT=8080
+export PORT=8080
 export GOOGLE_ANALYTICS="UA-11532471-6"
 export CHIVE_KEY="kBClwk1fEiyo3wGiPT7g4dfbVpTuoIlz"
 
 
 if [ "$1" = "start" ]
 then
-  exec java -cp target/nossal.jar clojure.main -m nossal.app
+  ls -l
+  exec java $JVM_OPTS -cp target/nossal.jar clojure.main -m nossal.app
 fi
 
 exec "$@"
