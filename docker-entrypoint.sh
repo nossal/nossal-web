c0da7903af2eb17d093a44d4e9be3efb95a6d3b9
nossal <rodrigo@noss.al>
2017-10-09 01:01:00 -0300

    no port var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -ex
 
 export JVM_OPTS="-Xms50m -Xmx100m -Dfile.encoding=UTF-8"
-export PORT=8080
+# export PORT=8080
 export GOOGLE_ANALYTICS="UA-11532471-6"
 export CHIVE_KEY="kBClwk1fEiyo3wGiPT7g4dfbVpTuoIlz"
 
