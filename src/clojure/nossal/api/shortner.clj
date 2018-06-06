c243fdf84fb5385382f036f2a58e0888981e0aba
Rodrigo Nossal <rodrigo@noss.al>
2018-06-06 00:55:57 -0300

    proto crud

--- src/clojure/nossal/api/shortner.clj
+++ src/clojure/nossal/api/shortner.clj
@@ -3,8 +3,15 @@
             [environ.core :refer [env]]
             [clojure.data.json :as json]
             [ring.util.response :as res]
+            [nossal.db :as data]
+            [nossal.db :refer [db]]))
 
 
 (defn create-database []
   (-> (res/response (json/write-str (data/create-urls-table db)))
+      (res/content-type "text/plain")))
+
+
+(defn new-url [url]
+  (-> (res/response (json/write-str (data/insert-url db {:url url})))
       (res/content-type "text/plain")))
