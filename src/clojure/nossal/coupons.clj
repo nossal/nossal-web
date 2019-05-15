eacdd75ba8f2f4b5ff971dcf8124885bd1cd1cac
nossal <rodrigo@noss.al>
2019-05-15 16:13:13 +0000

    warn 99 expiration

--- src/clojure/nossal/coupons.clj
+++ src/clojure/nossal/coupons.clj
@@ -41,7 +41,7 @@
              :description "💲 Ganhe R$10,00 de desconto na primeira corrida 99 (99POP também)! ✅"
              :text [:p "Ganhe " [:span.value "R$10,00"]" de desconto na sua " [:strong "primeira"] " viagem no 99! " [:ruby "🎉 " [:rt "ta-da!"]]
                      [:small"Este cupom pode ser utilizado apenas uma vez por pessoa, em sua primeira corrida. Ganhe até " [:span.value "100%"] " de desconto na sua viagem."]
-                     [:p.warn [:span "Seja rápido!"] " você tem " [:strong "até 15 dias"] "para fazer a sua primeira viagem antes do cupom expirar. 😨"]]}
+                     [:p.warn [:span "Seja rápido!"] " Você tem " [:strong "até 15 dias"] "para fazer a sua primeira viagem antes do cupom expirar. 😨"]]}
    "picpay" {:code "PKTA9D"
              :title ["PicPay"]
              :url "http://www.picpay.com/convite?!PKTA9D"
